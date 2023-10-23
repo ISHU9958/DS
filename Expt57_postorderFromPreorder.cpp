4b190d1bea4a8a4e05b13c189edbf198a5bfe8d9
ISHU9958 <147924001+ISHU9958@users.noreply.github.com>
2023-10-23 22:39:30 +0530

    Update Expt57_postorderFromPreorder.cpp

--- Expt57_postorderFromPreorder.cpp
+++ Expt57_postorderFromPreorder.cpp
@@ -1,28 +1,33 @@
-#include <iostream>
+#include <bits/stdc++.h>
 #include <vector>
 
 using namespace std;
 
-struct TreeNode {
+struct TreeNode
+{
     int val;
-    TreeNode* left;
-    TreeNode* right;
+    TreeNode *left;
+    TreeNode *right;
     TreeNode(int value) : val(value), left(nullptr), right(nullptr) {}
 };
 
-TreeNode* buildTreeFromPreorder(vector<int>& preorder, int& index, int maxValue) {
-    if (index >= preorder.size() || preorder[index] >= maxValue) {
+TreeNode *buildTreeFromPreorder(vector<int> &preorder, int &index, int maxValue)
+{
+    if (index >= preorder.size() || preorder[index] >= maxValue)
+    {
         return nullptr;
     }
 
-    TreeNode* root = new TreeNode(preorder[index++]);
+    TreeNode *root = new TreeNode(preorder[index++]);
     root->left = buildTreeFromPreorder(preorder, index, root->val);
     root->right = buildTreeFromPreorder(preorder, index, maxValue);
     return root;
 }
 
-void postorderTraversal(TreeNode* root) {
-    if (root == nullptr) {
+void postorderTraversal(TreeNode *root)
+{
+    if (root == nullptr)
+    {
         return;
     }
 
@@ -31,12 +36,13 @@ void postorderTraversal(TreeNode* root) {
     cout << root->val << " ";
 }
 
-int main() {
+int main()
+{
     vector<int> preorder = {1, 2, 4, 5, 3, 6, 7};
     int n = 0;
     int maxValue = INT_MAX;
 
-    TreeNode* root = buildTreeFromPreorder(preorder, n, maxValue);
+    TreeNode *root = buildTreeFromPreorder(preorder, n, maxValue);
 
     postorderTraversal(root);
     cout << endl;
