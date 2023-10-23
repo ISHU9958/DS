c22291ebe786139f6f1f0a33e0010738970c60f8
ISHU9958 <147924001+ISHU9958@users.noreply.github.com>
2023-10-23 21:59:33 +0530

    Update Expt29_stackImplementationArray.cpp

--- Expt29_stackImplementationArray.cpp
+++ Expt29_stackImplementationArray.cpp
@@ -1,60 +1,74 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 // stack class
-class stack{
+class stack
+{
     // private data members
-    private:
+private:
     int top;
     int *arr;
     int size;
 
     // public member functions
-    public:
-    //constructor
-    stack(int size){
-        this->size=size;
-        arr=new int[size];
-        top=-1;
+public:
+    // constructor
+    stack(int size)
+    {
+        this->size = size;
+        arr = new int[size];
+        top = -1;
     }
 
     // push operation
-    void push(int val){
-        if(size-top>1){
+    void push(int val)
+    {
+        if (size - top > 1)
+        {
             top++;
-            arr[top]=val;
+            arr[top] = val;
         }
-        else{
-            cout<<"stack is overflow "<<endl;
+        else
+        {
+            cout << "stack is overflow " << endl;
         }
     }
 
     // pop operation
-    void pop(){
-        if(top==-1){
-            cout<<"stack is overflow "<<endl;
+    void pop()
+    {
+        if (top == -1)
+        {
+            cout << "stack is overflow " << endl;
         }
-        else{
+        else
+        {
             top--;
         }
     }
 
     // isempty
-    bool empty(){
-        return top==-1 ? true:false;
+    bool empty()
+    {
+        return top == -1 ? true : false;
     }
     // get peek element
-    int peek(){
-        return top!=-1 ? arr[top] : -1;
+    int peek()
+    {
+        return top != -1 ? arr[top] : -1;
     }
 };
-int main(){
+int main()
+{
 
     stack st(5);
     st.push(1);
     st.push(2);
-    while(st.emtpy()){
-        cout<<st.peek()<<" ";
+    cout << "stack elements are : ";
+   
+    while (!st.empty())
+    {
+        cout << st.peek() << " ";
         st.pop();
     }
     return 0;
